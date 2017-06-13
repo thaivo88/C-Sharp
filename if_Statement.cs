fc0ccaa6d21e8730bbdf10cb72db058a5a4f401c
thai vo <thaivo88@gmail.com>
2017-06-13 09:25:58 -0500

    Create if_Statement.cs

---
+++ if_Statement.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace if_Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 10;
+            int number2 = 20;
+            if (number2 > number1){
+                Console.WriteLine("Number 2 is greater than number1");
+            }
+        }
+    }
+}
