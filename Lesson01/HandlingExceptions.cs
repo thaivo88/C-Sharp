753a2046e803a798280bb34e147c88ec9728b47f
thai vo <thaivo88@gmail.com>
2017-06-14 10:17:38 -0500

    Create HandlingExceptions.cs

--- Lesson01/HandlingExceptions.cs
+++ Lesson01/HandlingExceptions.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO; //the StreamReader is part of this class
 
 namespace HandlingException
 {
